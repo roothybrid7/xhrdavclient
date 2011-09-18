266a9e439868d2066a8c2467a340b329bdfdab81
Satoshi Ohki <roothybrid7@gmail.com>
2011-09-19 03:39:14 +0900

    Add generate deps.js

---
+++ tools/depsgen.sh
@@ -0,0 +1,25 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  depsgen.sh
+# 
+#         USAGE:  ./depsgen.sh 
+# 
+#   DESCRIPTION:  Generate Closure library depend difinition script file.
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: Satoshi Ohki (roothybrid7[at]gmail.com),
+#       COMPANY: 
+#       CREATED: 2011/09/19 03時24分53秒 JST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+PYTHON=$(which python)
+
+$PYTHON closure-library/closure/bin/build/depswriter.py --root_with_prefix="scripts ../../../scripts" --output_file=deps.js
+[ $? -ne 0 ] && echo "USAGE: ./tools/$(basename $0)"
