755fceb9a7337d0a4231e1eda64ee16f79ff0fac
Satoshi Ohki <roothybrid7@gmail.com>
2011-09-27 12:58:59 +0900

    Add static functions script

---
+++ scripts/functions.js
@@ -0,0 +1,11 @@
+/**
+ * function.js - XHTTPRequest High-level WebDAV Client API.
+ *
+ * @license Copyright 2011 The xhrdavclient library authors. All rights reserved.
+ */
+
+goog.provide('xhrdav.lib.Functions');
+goog.require('xhrdav.lib.Config');
+
+xhrdav.lib.Functions.StaticFunc = function() {
+};
