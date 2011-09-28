4fe37bab77b0d35887eb9f07dce139cdc2ad2e21
Satoshi Ohki <roothybrid7@gmail.com>
2011-09-28 23:25:40 +0900

    update testdata

--- scripts/config.js
+++ scripts/config.js
@@ -5,11 +5,21 @@
  */
 
 goog.provide('xhrdav.lib.Config');
-goog.require('goog.functions');
-goog.require('goog.array');
 goog.require('goog.object');
+goog.require('xhrdav.lib.string');
 goog.require('goog.string.path');
 goog.require('xhrdav.lib.HttpStatus');
 goog.require('goog.Disposable');
 goog.require('goog.debug.Console');
 goog.require('goog.debug.Logger');
+
+/**
+ * Namespace shorthand
+ *
+ * @type {Object}
+ */
+var $xdav = xhrdav.lib;
+
+/* Entry Point for closure compiler */
+goog.exportSymbol('$xdav', xhrdav.lib);
+
---
+++ scripts/core_ext/string.js
@@ -0,0 +1,58 @@
+/**
+ * string.js - string prototype extensions
+ *
+ * @license Copyright 2011 The xhrdavclient library authors. All rights reserved.
+ */
+
+goog.provide('xhrdav.lib.string');
+goog.require('goog.array');
+goog.require('goog.functions');
+
+/**
+ * Letter type enum(use camelize param etc.)
+ *
+ * @enum {number}
+ */
+xhrdav.lib.string.LetterType = {
+  LOWER: 0,
+  UPPER: 1
+};
+
+/**
+ * Capitalize string
+ *
+ * @return {string} Capitalized string.
+ */
+String.prototype.$capitalize = function() {
+  return (this.charAt(0).toUpperCase() + this.slice(1));
+};
+
+/**
+ * Camelize string
+ *
+ * @param {number} firstLetter type(Lower OR Upper)
+ * @return {string} Camelized string.
+ */
+String.prototype.$camelize = function(firstLetter) {
+  var self = this;
+  var lType = xhrdav.lib.string.LetterType;
+  if (!goog.isDefAndNotNull(firstLetter) || firstLetter > lType.UPPER) {
+    firstLetter = lType.LOWER;
+  }
+
+  var str = '';
+  if (firstLetter == lType.LOWER) {
+    str = goog.array.reduce(this.split('_'), function(result, v, i) {
+      return (result += (i == 0) ? v : v.$capitalize());
+    }, '');
+  } else if (firstLetter == lType.UPPER) {
+    str = goog.array.reduce(this.split('_'), function(result, v, i) {
+      return result += v.$capitalize();
+    }, '');
+  }
+  str = goog.array.reduce(str.split('/'), function(result, v, i) {
+    return (result += (i == 0) ? v : '.' + v.$capitalize());
+  }, '');
+
+  return str;
+};
