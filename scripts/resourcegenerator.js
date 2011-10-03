c5cc5d6220c0a9816191ee720547d9eb9ca940e3
Satoshi Ohki <roothybrid7@gmail.com>
2011-10-03 11:46:33 +0900

    Add rename method in ResoruceGenerator

--- scripts/resourcegenerator.js
+++ scripts/resourcegenerator.js
@@ -66,12 +66,13 @@ xhrdav.lib.ResourceGenerator.prototype.getDestination = function() {
  *                                        Json/Hash object for WebDAV resoruce.
  * @param {Function=} handler callback handler function
  *                            [callback args: errors object]
- * @param {Object=} opt_request Request options.
+ * @param {Object=} opt_headers Request headers.
+ * @param {object=} opt_params  Request query params.
  * @param {Fuction=} debugHandler [Callback args: xhr event object]
  * @throws {Error} Not found of xhrdav.lib.Resource or #destination
  */
 xhrdav.lib.ResourceGenerator.remove = function(
-  resource, handler, opt_request, debugHandler) {
+  resource, handler, opt_headers, opt_params, debugHandler) {
   // TODO: Implements
 };
 
@@ -80,13 +81,14 @@ xhrdav.lib.ResourceGenerator.remove = function(
  *
  * @param {Function=} handler callback handler function
  *                            [callback args: errors object]
- * @param {Object=} opt_request Request options.
+ * @param {Object=} opt_headers Request headers.
+ * @param {object=} opt_params  Request query params.
  * @param {Fuction=} debugHandler [Callback args: xhr event object]
  * @throws {Error} Not found of xhrdav.lib.Resource or #destination
  * @see xhrdav.lib.ResourceGenerator.remove
  */
 xhrdav.lib.ResourceGenerator.prototype.remove = function(
-  handler, opt_request, debugHandler) {
+  handler, opt_headers, opt_params, debugHandler) {
   // TODO: Implements
 };
 
@@ -98,12 +100,13 @@ xhrdav.lib.ResourceGenerator.prototype.remove = function(
  * @param {string} dest Copy destination Path <code>/mydav/bar/</code>.
  * @param {Function=} handler callback handler function
  *                            [callback args: errors object]
- * @param {Object=} opt_request Request options.
+ * @param {Object=} opt_headers Request headers.
+ * @param {object=} opt_params  Request query params.
  * @param {Fuction=} debugHandler [Callback args: xhr event object]
  * @throws {Error} Not found of xhrdav.lib.Resource or #destination
  */
 xhrdav.lib.ResourceGenerator.copy = function(
-  resource, dest, handler, opt_request, debugHandler) {
+  resource, dest, handler, opt_headers, opt_params, debugHandler) {
 //  if (!this.destination) // Errors
   // TODO: Implements
 };
@@ -113,13 +116,14 @@ xhrdav.lib.ResourceGenerator.copy = function(
  *
  * @param {Function=} handler callback handler function
  *                            [callback args: errors object]
- * @param {Object=} opt_request Request options.
+ * @param {Object=} opt_headers Request headers.
+ * @param {object=} opt_params  Request query params.
  * @param {Fuction=} debugHandler [Callback args: xhr event object]
  * @throws {Error} Not found of xhrdav.lib.Resource or #destination
  * @see xhrdav.lib.ResourceGenerator.copy
  */
 xhrdav.lib.ResourceGenerator.prototype.copy = function(
-  handler, opt_request, debugHandler) {
+  handler, opt_headers, opt_params, debugHandler) {
 //  if (!this.destination) // Errors
   // TODO: Implements
 };
@@ -132,12 +136,13 @@ xhrdav.lib.ResourceGenerator.prototype.copy = function(
  * @param {string} dest Move destination Path <code>/mydav/bar/</code>.
  * @param {Function=} handler callback handler function
  *                            [callback args: errors object]
- * @param {Object=} opt_request Request options.
+ * @param {Object=} opt_headers Request headers.
+ * @param {object=} opt_params  Request query params.
  * @param {Fuction=} debugHandler [Callback args: xhr event object]
  * @throws {Error} Not found of xhrdav.lib.Resource or #destination
  */
 xhrdav.lib.ResourceGenerator.move = function(
-  resoruce, dest, handler, opt_request, debugHandler) {
+  resoruce, dest, handler, opt_headers, opt_params, debugHandler) {
 //  if (!this.destination) // Errors
   // TODO: Implements
 };
@@ -147,16 +152,51 @@ xhrdav.lib.ResourceGenerator.move = function(
  *
  * @param {Function=} handler callback handler function
  *                            [callback args: errors object]
- * @param {Object=} opt_request Request options.
+ * @param {Object=} opt_headers Request headers.
+ * @param {object=} opt_params  Request query params.
  * @param {Fuction=} debugHandler [Callback args: xhr event object]
  * @throws {Error} Not found of Resource or #destination
  * @see xhrdav.lib.ResourceGenerator.move
  */
 xhrdav.lib.ResourceGenerator.prototype.move = function(
-  handler, opt_request, debugHandler) {
+  handler, opt_headers, opt_params, debugHandler) {
 //  if (!this.destination) // Errors
   // TODO: Implements
 }
+
+/**
+ * Rename resource [Class method]
+ *
+ * @param {(xhrdav.lib.ResourceGenerator|xhrdav.lib.Resource)} resource
+ *                                        Json/Hash object for WebDAV resource.
+ * @param {string} dest New resource name.
+ * @param {Function=} handler callback handler function
+ *                            [callback args: errors object]
+ * @param {Object=} opt_headers Request headers.
+ * @param {object=} opt_params  Request query params.
+ * @param {Function=} debugHandler  [Callback args: errors object]
+ * @throws {Error} Not found of xhrdav.lib.Resource or #destination
+ */
+xhrdav.lib.ResourceGenerator.rename = function(
+  resource, dest, handler, opt_headers, opt_params, debugHandler) {
+  // TODO: Implements
+};
+
+/**
+ * Rename resource
+ *
+ * @param {Function=} handler callback handler function
+ *                            [callback args: errors object]
+ * @param {Object=} opt_headers Request headers.
+ * @param {object=} opt_params  Request query params.
+ * @param {Function=} debugHandler  [Callback args: errors object]
+ * @throws {Error} Not found of xhrdav.lib.Resource or #destination
+ */
+xhrdav.lib.ResourceGenerator.prototype.rename = function(
+  handler, opt_headers, opt_params, debugHandler) {
+  // TODO: Implements
+};
+
 
 /* Entry point for closure compiler */
 goog.exportSymbol('xhrdav.lib.ResourceGenerator', xhrdav.lib.ResourceGenerator);
