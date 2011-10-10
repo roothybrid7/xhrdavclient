00559114ed2578b80046872847a270e15164b4cd
Satoshi Ohki <roothybrid7@gmail.com>
2011-10-10 17:54:38 +0900

    Add sendAsBinary in XMLHttpRequest and refactor example

--- scripts/config.js
+++ scripts/config.js
@@ -16,6 +16,7 @@ goog.require('goog.json');
 goog.require('goog.functions');
 goog.require('goog.string.path');
 goog.require('xhrdav.lib.functions');
+goog.require('xhrdav.lib.XmlHttp');
 goog.require('xhrdav.lib.Errors');
 goog.require('xhrdav.lib.string');
 goog.require('xhrdav.lib.HttpStatus');
