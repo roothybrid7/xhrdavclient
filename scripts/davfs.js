6f87ce9291657738cc5de12327b23e6615e7d5ef
Satoshi Ohki <roothybrid7@gmail.com>
2011-10-16 16:43:10 +0900

    Add DavFs.Request method

--- scripts/davfs.js
+++ scripts/davfs.js
@@ -1,7 +1,9 @@
 /**
- * davfs.js - XHTTPRequest High-level WebDAV Client API.
+ * davfs.js - WebDAV settings and XHTTPRequest object Management API and
+ *            XHTTPRequest High-level WebDAV Client API.
  *
- * This is a High-level WebDAV client API for File system.
+ * This is a Manager of WebDAV and XHTTPRequest for File system.
+ * and a High-level WebDAV client API for File system.
  * Path base request.
  * Resource base request: @see xhrdav.lib.ResourceController
  *
@@ -599,10 +601,10 @@ xhrdav.lib.DavFs.prototype.getRequest = function(
  * An encapsulation of everything needed to make a DavFs request.
  *
  *
+ * @constructor
  * @param {xhrdav.lib.Client} davSite WebDAV site.
  * @param {(goog.net.XhrIo|goog.net.XhrManager)} xhrIo request object
  *     of closure library (For Cross-site resource sharing[CORS]).
- * @constructor
  * @extends {goog.Disposable}
  */
 xhrdav.lib.DavFs.Request = function(davSite, xhrIo) {
@@ -611,13 +613,479 @@ xhrdav.lib.DavFs.Request = function(davSite, xhrIo) {
   /** @type {xhrdav.lib.Client} */
   this.davSite_ = davSite;
   /** @type {(goog.net.XhrIo|goog.net.XhrManager)} */
-  this.xhrIo = xhrIo;
+  this.xhrIo_ = xhrIo;
 };
 goog.inherits(xhrdav.lib.DavFs.Request, goog.Disposable);
 
 /** @inheritDoc */
 xhrdav.lib.DavFs.Request.prototype.disposeInternal = function() {
   xhrdav.lib.DavFs.Request.superClass_.disposeInternal.call(this);
+};
+
+/**
+ * WebDAV Response process handler(callback)
+ *
+ * @private
+ * @param {Function} handler callback client.
+ * @param {Function} processHandler
+ * @param {number} statusCode HTTP Status code.
+ * @param {Object} content Response body data.
+ * @param {Object} headers Response headers.
+ */
+xhrdav.lib.DavFs.Request.prototype.responseHandler_ = function(
+  handler, processHandler, path, context, statusCode, content, headers) {
+  var httpStatus = xhrdav.lib.HttpStatus;
+  var args = processHandler(path, statusCode, content, headers);
+  if (goog.isDefAndNotNull(context) && goog.isObject(context)) {
+    handler.apply(context, args);
+  } else {
+    handler(args);
+  }
+};
+
+/**
+ * Content read Handler
+ *
+ * @private
+ * @param {string} path HTTP Request path.
+ * @param {number} statusCode HTTP Status code.
+ * @param {Object} content Response body data.
+ * @param {Object} headers Response headers.
+ * @return {Array.<xhrdav.lib.Errors, string=>} Errors, new Location
+ * @see xhrdav.lib.Errors
+ */
+xhrdav.lib.DavFs.Request.prototype.contentReadHandler_ = function(
+  path, statusCode, content, headers) {
+  var config = xhrdav.lib.Config.getInstance();
+  var httpStatus = xhrdav.lib.HttpStatus;
+  var errors = new xhrdav.lib.Errors();
+
+  var args = [];
+  if (statusCode != httpStatus.OK) {
+    errors.setRequest({status: statusCode, message: httpStatus.text[statusCode], path: path});
+  }
+  args.push(errors);
+  args.push(content);
+
+  return args;
+};
+
+/**
+ * Exists Handler
+ *
+ * @private
+ * @param {string} path HTTP Request path.
+ * @param {number} statusCode HTTP Status code.
+ * @param {Object} content Response body data.
+ * @param {Object} headers Response headers.
+ * @return {Array.<xhrdav.lib.Errors, boolean>} Errors, new Location
+ * @see xhrdav.lib.Errors
+ */
+xhrdav.lib.DavFs.Request.prototype.existsHandler_ = function(
+  path, statusCode, content, headers) {
+  var config = xhrdav.lib.Config.getInstance();
+  var httpStatus = xhrdav.lib.HttpStatus;
+  var errors = new xhrdav.lib.Errors();
+
+  var args = [];
+  if (!goog.array.contains(
+    [httpStatus.OK, httpStatus.CREATED, httpStatus.NO_CONTENT],
+    statusCode)) {
+    errors.setRequest(
+      {status: statusCode, message: httpStatus.text[statusCode], path: path});
+  }
+  args.push(errors);
+  args.push(errors.hasRequest() ? false : true);
+  return args;
+};
+
+/**
+ * Error Handler
+ *
+ * @private
+ * @param {string} path HTTP Request path.
+ * @param {number} statusCode HTTP Status code.
+ * @param {Object} content Response body data.
+ * @param {Object} headers Response headers.
+ * @return {Array.<xhrdav.lib.Errors, string=>} Errors, new Location
+ * @see xhrdav.lib.Errors
+ */
+xhrdav.lib.DavFs.Request.prototype.simpleErrorHandler_ = function(
+  path, statusCode, content, headers) {
+  var config = xhrdav.lib.Config.getInstance();
+  var httpStatus = xhrdav.lib.HttpStatus;
+  var errors = new xhrdav.lib.Errors();
+
+  var args = [];
+  if (!goog.array.contains(
+    [httpStatus.OK, httpStatus.CREATED, httpStatus.NO_CONTENT],
+    statusCode)) {
+    errors.setRequest(
+      {status: statusCode, message: httpStatus.text[statusCode], path: path});
+  }
+  args.push(errors);
+  if (statusCode == httpStatus.CREATED) {
+    args.push(headers['Location']);
+  }
+  return args;
+};
+
+/**
+ * Build Directory tree from multistatus
+ *
+ * @param {Object} content  multistatus response data.
+ * @param {Object=} opt_helper Returning resource controller.
+ * @return {(xhrdav.lib.Resource|xhrdav.lib.ResourceController|Object)}
+ *         converted object for WebDAV resources.
+ * @see xhrdav.lib.ResourceBuilder.createCollection
+ */
+xhrdav.lib.DavFs.Request.getListDirFromMultistatus = function(content, opt_helper) {
+  if (!goog.isDefAndNotNull(opt_helper)) opt_helper = {};
+  var builder = xhrdav.lib.ResourceBuilder.createCollection(content);
+  return (!!opt_helper.hasCtrl) ?
+    builder.getResources() : builder.serialize(opt_helper.asModel);
+};
+
+/**
+ * Update(move, copy) request handler.
+ *
+ * @param {string} method Method name of xhrdav.lib.Client instance.
+ * @param {string} path Update src file path.
+ * @param {string} dstPath Update destination path.
+ * @param {Function} handler  callback handler function.
+ * @param {Object=} opt_headers Request headers options.
+ * @param {Object=} opt_params  Request query paramters.
+ * @param {Object=} context Callback scope.
+ * @param {Function=} onXhrComplete onXhrComplete callback function
+ */
+xhrdav.lib.DavFs.Request.prototype.updateRequestHandler_ = function(
+  method, path, dstPath, handler, opt_request, context, onXhrComplete) {
+  var api = goog.getObjectByName(method, this.davSite_);
+
+  api.call(this.davSite_, path, dstPath,
+    goog.bind(this.responseHandler_, this,
+      handler, this.simpleErrorHandler_, path, context),
+    opt_request, onXhrComplete);
+};
+
+/**
+ * Propfind request(listDir, getProps) handler.
+ *
+ * @param {string} method Method name of xhrdav.lib.Client instance.
+ * @param {string} path propfind request path.
+ * @param {Function} handler  callback handler function.
+ * @param {Object=} opt_headers Request headers options.
+ * @param {Object=} opt_params  Request query paramters.
+ * @param {Object=} context Callback scope.
+ * @param {{hasCtrl:boolean, asModel:boolean}=} opt_helper  response options.
+ * @param {Function=} onXhrComplete onXhrComplete callback function
+ */
+xhrdav.lib.DavFs.Request.prototype.propfindRequestHandler_ = function(
+  path, handler, opt_request, context, opt_helper, onXhrComplete) {
+  var dataHandler = goog.bind(this.processMultistatus_, this, opt_helper);
+  this.davSite_.propfind(path,
+    goog.bind(this.responseHandler_, this, handler, dataHandler, path, context),
+    opt_request, onXhrComplete);
+};
+
+/**
+ * Processing Reponse Multi-Status Data
+ *
+ * @param {string} path HTTP Request path.
+ * @param {number} statusCode HTTP Status code.
+ * @param {Object} content Response body data.
+ * @param {Object} headers Response headers.
+ * @return {Array.<xhrdav.lib.Errors, xhrdav.lib.Response>}
+ * @see xhrdav.lib.Errors
+ */
+xhrdav.lib.DavFs.Request.prototype.processMultistatus_ = function(
+  opt_helper, path, statusCode, content, headers) {
+  var config = xhrdav.lib.Config.getInstance();
+  var httpStatus = xhrdav.lib.HttpStatus;
+  var errors = new xhrdav.lib.Errors();
+
+  var args = [];
+  if (statusCode == httpStatus.MULTI_STATUS) {
+    content =
+      xhrdav.lib.DavFs.Request.getListDirFromMultistatus(content, opt_helper);
+  } else {
+    errors.setRequest({status: statusCode, message: httpStatus.text[statusCode], path: path});
+  }
+  args.push(errors);
+  args.push(content);
+
+  return args;
+};
+
+/**
+ * Create Request paramters.
+ *
+ * @param {boolean} isMgr Add XhrManager object for request.
+ * @param {Object=} opt_headers
+ * @param {Object=} opt_params
+ * @return {Object}
+ */
+xhrdav.lib.DavFs.Request.prototype.createRequestParameters_ = function(
+  xhrIoMap, opt_headers, opt_params, opt_xhrId) {
+  var opt_request = {headers: opt_headers || {}, query: opt_params || {}};
+
+  if (!goog.isDefAndNotNull(this.xhrIo_)) {
+    xhrdav.lib.Config.logging(
+      {'DavFs.Request#createRequestParameters_': 'Not found xhrIo object!!'},
+      'warning');
+  } else if (this.xhrIo_ instanceof goog.net.XhrIo) {
+    goog.object.extend(opt_request,{xhrIo: this.xhrIo_});
+  } else if (this.xhrIo_ instanceof goog.net.XhrManager) {
+    var map;
+    if (!goog.string.isEmptySafe(opt_xhrId)) {
+      map = {xhrId: opt_xhrId, xhrMgr: this.xhrIo_};
+    } else {
+      map = {xhrId: goog.string.createUniqueString(), xhrMgr: this.xhrIo_};
+    }
+    goog.object.extend(opt_request, map);
+  }
+
+  return opt_request;
+};
+
+/**
+ * listing collection
+ *
+ * @param {string} path
+ * @param {Function} handler callback handler function.
+ * @param {Object=} opt_headers Request headers options.
+ * @param {Object=} opt_params  Request query paramters.
+ * @param {Object=} context Callback scope.
+ * @param {{hasCtrl:boolean=, asModel:boolean=, xhrId:string=}=} opt_helper
+ *     xhr and response options.
+ * @param {Function=} onXhrComplete onXhrComplete callback function
+ * @see #propfindRequestHandler_
+ */
+xhrdav.lib.DavFs.Request.prototype.listDir = function(
+  path, handler, opt_headers, opt_params, context, opt_helper, onXhrComplete) {
+  var opt_request = this.createRequestParameters_(
+    opt_headers, opt_params, opt_helper && opt_helper.xhrId);
+
+  opt_request.headers['Depth'] = 1;  // listing directory
+  this.propfindRequestHandler_(path, handler, opt_request,
+    context, opt_helper, onXhrComplete);
+};
+
+/**
+ * Get property for a single resource.
+ *
+ * @param {string} path
+ * @param {Function} handler callback handler function.
+ * @param {Object=} opt_headers Request headers options.
+ * @param {Object=} opt_params  Request query paramters.
+ * @param {Object=} context Callback scope.
+ * @param {{hasCtrl:boolean=, asModel:boolean=, xhrId:string=}=} opt_helper
+ *     xhr and response options.
+ * @param {Function=} onXhrComplete onXhrComplete callback function
+ * @see #propfindRequestHandler_
+ */
+xhrdav.lib.DavFs.Request.prototype.getProps = function(
+  path, handler, opt_headers, opt_params, context, opt_helper, onXhrComplete) {
+  var opt_request = this.createRequestParameters_(
+    opt_headers, opt_params, opt_helper && opt_helper.xhrId);
+
+  this.propfindRequestHandler_(path, handler, opt_request, context, onXhrComplete);
+};
+
+/**
+ * Create directory (collection)
+ *
+ * @param {string} path Create dierctory path.
+ * @param {Function} handler callback handler function.
+ * @param {Object=} opt_headers Request headers options.
+ * @param {Object=} opt_params  Request query paramters.
+ * @param {Object=} context Callback scope.
+ * @param {{xhrId:string=}=} opt_helper xhr options.
+ * @param {Function=} onXhrComplete onXhrComplete callback function
+ */
+xhrdav.lib.DavFs.Request.prototype.mkDir = function(
+  path, handler, opt_headers, opt_params, context, opt_helper, onXhrComplete) {
+  var opt_request = this.createRequestParameters_(
+    opt_headers, opt_params, opt_helper && opt_helper.xhrId);
+
+  this.davSite_.mkcol(path,
+    goog.bind(this.responseHandler_, this,
+      handler, this.simpleErrorHandler_, path, context),
+    opt_request, onXhrComplete);
+};
+
+/**
+ * Remove resource
+ *
+ * @param {string} path Remove resource path.
+ * @param {Function} handler  callback handler function.
+ * @param {Object=} opt_headers Request headers options.
+ * @param {Object=} opt_params  Request query paramters.
+ * @param {Object=} context Callback scope.
+ * @param {{xhrId:string=}=} opt_helper xhr options.
+ * @param {Function=} onXhrComplete onXhrComplete callback function
+ */
+xhrdav.lib.DavFs.Request.prototype.remove = function(
+  path, handler, opt_headers, opt_params, context, opt_helper, onXhrComplete) {
+  var opt_request = this.createRequestParameters_(
+    opt_headers, opt_params, opt_helper && opt_helper.xhrId);
+
+  this.davSite_._delete(path,
+    goog.bind(this.responseHandler_, this,
+      handler, this.simpleErrorHandler_, path, context),
+    opt_request, onXhrComplete);
+};
+
+/**
+ * Move resource
+ *
+ * @param {string} path Move src resource path.
+ * @param {string} dstPath  Move destination path.
+ * @param {Function} handler  callback handler function.
+ * @param {Object=} opt_headers Request headers options.
+ * @param {Object=} opt_params  Request query paramters.
+ * @param {Object=} context Callback scope.
+ * @param {{xhrId:string=}=} opt_helper xhr options.
+ * @param {Function=} onXhrComplete onXhrComplete callback function
+ */
+xhrdav.lib.DavFs.Request.prototype.move = function(
+  path, dstPath, handler, opt_headers, opt_params, context,
+  opt_helper, onXhrComplete) {
+  var opt_request = this.createRequestParameters_(
+    opt_headers, opt_params, opt_helper && opt_helper.xhrId);
+
+  this.updateRequestHandler_('move',
+    path, dstPath, handler, opt_request, context, onXhrComplete);
+};
+
+/**
+ * Copy resource
+ *
+ * @param {string} path Move src resource path.
+ * @param {string} dstPath  Move destination path.
+ * @param {Function} handler  callback handler function.
+ * @param {Object=} opt_headers Request headers options.
+ * @param {Object=} opt_params  Request query paramters.
+ * @param {Object=} context Callback scope.
+ * @param {{xhrId:string=}=} opt_helper xhr options.
+ * @param {Function=} onXhrComplete onXhrComplete callback function
+ * @see #updateRequestHandler_
+ */
+xhrdav.lib.DavFs.Request.prototype.copy = function(
+  path, dstPath, handler, opt_headers, opt_params, context,
+  opt_helper, onXhrComplete) {
+  var opt_request = this.createRequestParameters_(
+    opt_headers, opt_params, opt_helper && opt_helper.xhrId);
+
+  this.updateRequestHandler_('copy',
+    path, dstPath, handler, opt_request, context, onXhrComplete);
+};
+
+/**
+ * Read data from WebDAV server
+ *
+ * @param {string} path read file path.
+ * @param {Function} handler callback handler function.
+ * @param {Object=} opt_headers Request headers options.
+ * @param {Object=} opt_params  Request query paramters.
+ * @param {{xhrId:string=}=} opt_helper xhr options.
+ * @param {Function=} onXhrComplete onXhrComplete callback function
+ */
+xhrdav.lib.DavFs.Request.prototype.read = function(
+  path, handler, opt_headers, opt_params, context, opt_helper, onXhrComplete) {
+  var opt_request = this.createRequestParameters_(
+    opt_headers, opt_params, opt_helper && opt_helper.xhrId);
+
+  path = xhrdav.lib.utils.path.removeLastSlash(path);
+
+  this.davSite_.get(path,
+    goog.bind(this.responseHandler_, this,
+      handler, this.contentReadHandler_, path, context),
+    opt_request, onXhrComplete);
+};
+
+/**
+ * Write data to WebDAV server
+ *
+ * @param {string} path upload file path.
+ * @param {Object} content data string.
+ * @param {Function} handler callback handler function.
+ * @param {Object=} opt_headers Request headers options.
+ * @param {Object=} opt_params  Request query paramters.
+ * @param {Object=} context Callback scope.
+ * @param {{xhrId:string=}=} opt_helper xhr options.
+ * @param {Function=} onXhrComplete onXhrComplete callback function
+ */
+xhrdav.lib.DavFs.Request.prototype.write = function(
+  path, content, handler, opt_headers, opt_params, context,
+  opt_helper, onXhrComplete) {
+  var opt_request = this.createRequestParameters_(
+    opt_headers, opt_params, opt_helper && opt_helper.xhrId);
+
+  path = xhrdav.lib.utils.path.removeLastSlash(path);
+
+  this.davSite_.put(path, content,
+    goog.bind(this.responseHandler_, this,
+      handler, this.simpleErrorHandler_, path, context),
+    opt_request, onXhrComplete);
+};
+
+/**
+ * Upload data to WebDAV server
+ *
+ * @param {string} path upload file path.
+ * @param {File} file File object(File API)
+ * @param {Function} handler callback handler function.
+ * @param {Object=} opt_headers Request headers options.
+ * @param {Object=} opt_params  Request query paramters.
+ * @param {Object=} context Callback scope.
+ * @param {{xhrId:string=}=} opt_helper xhr options.
+ * @param {Function=} onXhrComplete onXhrComplete callback function
+ */
+xhrdav.lib.DavFs.Request.prototype.upload = function(
+  path, file, handler, opt_headers, opt_params, context,
+  opt_helper, onXhrComplete) {
+  var opt_request = this.createRequestParameters_(
+    opt_headers, opt_params, opt_helper && opt_helper.xhrId);
+
+  path = xhrdav.lib.utils.path.removeLastSlash(path);
+  if (!(file instanceof File)) {
+    xhrdav.lib.Config.logging(
+      {'DavFs.Request#upload': 'Not a file object!![path: ' + path + ']'},
+      'warning');
+    xhrdav.lib.Config.logging(file, warning);
+  }
+  if (goog.isDefAndNotNull(file)) {
+    goog.object.extend(opt_request.headers,
+      {x_file_name: file.name, x_file_size: file.size});
+  }
+
+  this.davSite_.put(path, file,
+    goog.bind(this.responseHandler_, this,
+      handler, this.simpleErrorHandler_, path, context),
+    opt_request, onXhrComplete);
+};
+
+/**
+ * Resource Exists to WebDAV server
+ *
+ * @param {string} path exists resource path.
+ * @param {Function} handler callback handler function.
+ * @param {Object=} opt_headers Request headers options.
+ * @param {Object=} opt_params  Request query paramters.
+ * @param {Object=} context Callback scope.
+ * @param {{xhrId:string=}=} opt_helper xhr options.
+ * @param {Function=} onXhrComplete onXhrComplete callback function
+ */
+xhrdav.lib.DavFs.Request.prototype.exists = function(
+  path, handler, opt_headers, opt_params, context, opt_helper, onXhrComplete) {
+  var opt_request = this.createRequestParameters_(
+    opt_headers, opt_params, opt_helper && opt_helper.xhrId);
+
+  this.davSite_.head(path,
+    goog.bind(this.responseHandler_, this,
+      handler, this.existsHandler_, path, context),
+    opt_request, onXhrComplete);
 };
 
 
