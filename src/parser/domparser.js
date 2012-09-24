cb2ac1c02be50dd676b834cd7fd18e61c9e5baa3
Satoshi Ohki <roothybrid7@gmail.com>
2012-09-24 10:07:55 +0900

    fix bug

--- src/parser/domparser.js
+++ src/parser/domparser.js
@@ -28,7 +28,7 @@ xhrdav.parser.DomParser = function() {
  * @return {xhrdav.DomParser} self object.
  */
 xhrdav.parser.DomParser.prototype.initialize = function(xml, handler, options) {
-  if (!(goog.isDefAndNotNull(xml) && xml instanceof Document)) {
+  if (!(goog.isDefAndNotNull(xml))) {
     goog.functions.error('Xml document is NOT GIVEN!!')();
   }
   if (!goog.isDefAndNotNull(handler)) {
